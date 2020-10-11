631a6d78d399da12c31f3646d3bae667dfc52e3f
Arek Cios <arek123113@gmail.com>
2020-10-11 12:58:32 +0200

    Display data on the index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
+import { GetStaticProps } from 'next'
+import { useQuery } from 'react-query';
 
 export default function Home() {
   
@@ -8,27 +10,48 @@ export default function Home() {
 
   let answers;
   async function getAnswers() {
-    console.log({url});
-    const res = await fetch('/api/GetAnswers', {
-      method:'POST',
-      body: JSON.stringify({url}),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then(data => answers = data);
-    console.log(answers)
-    console.log(answers.data[0])
+    try {
+      console.log({url});
+      const res = await fetch('/api/GetAnswers', {
+       method:'POST',
+       body: JSON.stringify({url}),
+       headers: {
+         'Content-Type': 'application/json'
+       }
+     })
+     .then(response => response.json())
+     .then(data => answers = data);
+     //console.log(res);
+ 
+     return res;
+    }
+    catch {
+      
+    }
+    
   }
 
+  function makeAlert() {
+    if(url==""){
+      alert("The url field is empty.")
+    }
+  }
+
+  const { data } = useQuery([url], getAnswers)
   return (
     <div>
       <form>
         <input type="text" name="url" placeholder="Paste the quiz url here" onChange={(e) => setUrl(e.target.value)} />
         <br />
-        <input type="button" value="Get Answers" onClick={getAnswers} />
+        <input type="button" value="Get Answers" onClick={()=> {getAnswers(); makeAlert();}} />
       </form>
+      {data && data.data.map(function(d, idx){
+         return (<li key={idx}>
+           {d.question}
+           <br />
+           {d.answer}
+           </li>)
+       })}
     </div>
   );
 }
